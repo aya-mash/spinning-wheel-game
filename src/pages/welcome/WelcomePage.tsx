e492ba32468684e865dc58a3a6c1c345874c249c
AYABULELA YANELA MAHLATHINI <ayabulela_yanela.mahlathini@mercedes-benz.com>
2025-04-10 16:42:53 +0200

    feat: creating reusable stylesheet and styling welcome page

--- src/pages/welcome/WelcomePage.tsx
+++ src/pages/welcome/WelcomePage.tsx
@@ -5,17 +5,22 @@ const Welcome = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div>
-        <div>
+    <div className="min-h-screen bg-gradient-welcome flex items-center justify-center min-w-screen">
+      <div className="card bg-white shadow-lg rounded-lg p-8 text-center">
+        <div className="icon icon-purple">
           <Sparkles />
         </div>
-        <h1>Spin & Win!</h1>
-        <p>
+        <h1 className="title">Spin & Win!</h1>
+        <p className="subtitle">
           Try your luck with our exciting spinning wheel game. Will fortune
           favor you today?
         </p>
-        <button onClick={() => navigate("/game")}> Get Started</button>
+        <button
+          onClick={() => navigate("/game")}
+          className="button button-welcome"
+        >
+          Get Started
+        </button>
       </div>
     </div>
   );
