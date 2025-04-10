2e2d1de14dc677bc640a4c4af4348726ac0c2d01
AYABULELA YANELA MAHLATHINI <ayabulela_yanela.mahlathini@mercedes-benz.com>
2025-04-10 17:08:59 +0200

    feat: adding game route to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/welcome/WelcomePage";
+import Game from "./pages/game/GamePage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Welcome />} />
+        <Route path="/game" element={<Game />} />
       </Routes>
     </Router>
   );
