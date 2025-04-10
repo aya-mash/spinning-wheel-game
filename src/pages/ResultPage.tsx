c7d9006d3ac0c173fc0b90c8d965814e1902fe26
AYABULELA YANELA MAHLATHINI <ayabulela_yanela.mahlathini@mercedes-benz.com>
2025-04-10 14:31:16 +0200

    feat: setup react-router with 3 core pages

---
+++ src/pages/ResultPage.tsx
@@ -0,0 +1,10 @@
+const ResultPage = () => {
+  return (
+    <div>
+      <h1>Result Page</h1>
+      <p>This is a placeholder for the result page.</p>
+    </div>
+  );
+};
+
+export default ResultPage;
---
+++ src/pages/WelcomePage.tsx
@@ -0,0 +1,10 @@
+const WelcomePage = () => {
+  return (
+    <div>
+      <h1>Welcome Page</h1>
+      <p>This is a placeholder for the welcome page.</p>
+    </div>
+  );
+};
+
+export default WelcomePage;
